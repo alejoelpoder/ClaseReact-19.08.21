ab6b8b2ac7cc2200e41d32b86a308d6b3aa2e8e2
unknown <alejomayuri@gmail.com>
2021-09-30 13:43:52 -0500

    Idea + Mayurí | Desafio clase 11

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,14 +1,47 @@
 import { useCartContext } from "../context/cartContext";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
 
     const { cartList, borrarProducto, borrarCarrito } = useCartContext()
 
+    const [precio, setPrecio] = useState(0)
+
+    const darPrecioTotal = () => {
+        const precioArr = []
+        let suma = 0
+        for (let i = 0; i < cartList.length; i++) {
+            precioArr[i] = cartList[i].item.precio * cartList[i].cantidad;
+        }
+        precioArr.forEach (function(num) {
+            suma += num;
+        })
+        setPrecio(suma)
+    }
+
+    useEffect(() => { 
+        darPrecioTotal()
+    }, [cartList]);
+
     return(
         <>
-            {cartList.map(item => <><h2>{item.item.titulo}</h2><span>{item.cantidad}</span><button onClick={() => borrarProducto(item)}>Borrar producto</button></>)}
-            <br />
-            <button onClick={borrarCarrito}>Borrar carrito</button>
+            {
+                Object.keys(cartList).length !== 0 ?
+                <>
+                    {cartList.map(item => <><h2>{item.item.titulo}</h2><span>{`$${item.item.precio} x `}</span><span>{item.cantidad}</span><button onClick={() => borrarProducto(item)}>Borrar producto</button></>)}
+                    <br /><br />
+                    {`El precio total es: $${precio}`}
+                    <br /><br />
+                    <button onClick={borrarCarrito}>Borrar carrito</button>
+                </>:
+                <>
+                    <p>No hay productos en el carrito</p>
+                    <Link to='/'>
+                        <button>Regresar</button>
+                    </Link>
+                </>
+            }
         </>
     )
 }
--- src/components/WidgetCart/index.jsx
+++ src/components/WidgetCart/index.jsx
@@ -1,15 +1,40 @@
 import "./WidgetCart.css";
 import { Link } from "react-router-dom";
+import { useCartContext } from "../context/cartContext";
+import { useState, useEffect } from "react";
 
 const WidgetCart = () => {
+
+    const { cartList } = useCartContext()
+
+    const [cantidad, setCantidad] = useState()
+
+    const darCantidad = () => {
+        const cantidadArr = []
+        let suma = 0
+        for (let i = 0; i < cartList.length; i++) {
+            cantidadArr[i] = cartList[i].cantidad;
+        }
+        cantidadArr.forEach (function(num) {
+            suma += num;
+        })
+        console.log(suma)
+        setCantidad(suma)
+        console.log()
+    }
+
+    useEffect(() => { 
+        darCantidad()
+    }, [cartList]);
+
     return(
-        <div className="widgetCart">
+        <div className={`widgetCart ${cantidad? 'cart-open' : 'cart-close'}`}>
             <Link to={'/Cart'}>
                 <img src="https://image.flaticon.com/icons/png/512/3144/3144456.png" alt="widget cart" />
             </Link>
-            {/* <a href="#">
-                <img src="https://image.flaticon.com/icons/png/512/3144/3144456.png" alt="widget cart" />
-            </a> */}
+            <span className="cantidadProductos">
+                {cantidad}
+            </span>
         </div>
     )
 }
